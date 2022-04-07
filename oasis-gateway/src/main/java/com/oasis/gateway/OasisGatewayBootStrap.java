8166a1fde7cd3886074a4dac998d9cc8d3aa08ad
neal <asas20@126.com>
2022-04-07 18:33:08 +0800

    创建目录以及ER图

---
+++ oasis-gateway/src/main/java/com/oasis/gateway/OasisGatewayBootStrap.java
@@ -0,0 +1,18 @@
+package com.oasis.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName OasisGatewayBootStrap
+ * @Description 网关启动类
+ * @Author zhushaobin
+ * @Date 2022/4/7 17:27
+ */
+@SpringBootApplication
+public class OasisGatewayBootStrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OasisGatewayBootStrap.class);
+    }
+}
