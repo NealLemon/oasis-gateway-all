6a54fb6ae0e4d03f30b95d6ae27c2b8f16b84dd4
neal <asas20@126.com>
2022-05-30 22:09:07 +0800

    提取 提高可读性

--- oasis-gateway/src/main/java/com/oasis/gateway/filter/factory/JsonHandleGatewayFilterFactory.java
+++ oasis-gateway/src/main/java/com/oasis/gateway/filter/factory/JsonHandleGatewayFilterFactory.java
@@ -1,24 +1,22 @@
 package com.oasis.gateway.filter.factory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.github.fge.jsonschema.core.exceptions.ProcessingException;
 import com.github.fge.jsonschema.core.load.Dereferencing;
 import com.github.fge.jsonschema.core.load.configuration.LoadingConfiguration;
 import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
-import com.github.fge.jsonschema.main.JsonValidator;
 import com.oasis.gateway.filter.support.dto.JsonHandleDTO;
-import com.oasis.gateway.util.ResponseUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
-import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
-import org.springframework.http.MediaType;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.core.Ordered;
 import org.springframework.http.codec.HttpMessageReader;
 import org.springframework.web.reactive.function.server.HandlerStrategies;
 import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -48,24 +46,63 @@ public class JsonHandleGatewayFilterFactory extends OasisAbstractGatewayFilterFa
 
     @Override
     public GatewayFilter apply(Config config) {
-        try {
-            JsonHandleDTO jsonHandleDTO = objectMapper.readValue(config.getConfiguration(),JsonHandleDTO.class);
-            JsonNode jsonSchema
-                    = jsonHandleDTO.getRequestJsonSchema();
-            JsonSchema validateSchema = jsonSchemaInlineFactory.getJsonSchema(jsonSchema);
-            return new OrderedGatewayFilter(((exchange, chain) -> {
+        return new JsonHandlerDefaultPlugin(config);
+    }
+
+    public class JsonHandlerDefaultPlugin implements  GatewayFilter, Ordered {
+
+        private final Config config;
+
+        public JsonHandlerDefaultPlugin(Config config) {
+            this.config = config;
+        }
+
+        @Override
+        public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+            try {
+                JsonHandleDTO jsonHandleDTO = objectMapper.readValue(config.getConfiguration(), JsonHandleDTO.class);
+                JsonNode jsonSchema = jsonHandleDTO.getRequestJsonSchema();
+                JsonSchema validateSchema = jsonSchemaInlineFactory.getJsonSchema(jsonSchema);
                 ServerRequest serverRequest = ServerRequest.create(exchange, messageReaders);
                 return serverRequest.bodyToMono(JsonNode.class).flatMap(jsonNode -> {
                     ProcessingReport report =  validateSchema.validateUnchecked(jsonNode);
                         if(report.isSuccess()) {
                             return chain.filter(exchange);
                         }
-                        return ResponseUtils.responseBody(exchange, MediaType.APPLICATION_JSON,"error");
+                      return Mono.error(new Exception(report.iterator().next().getMessage()));
                 });
-            }),10);
-        } catch (Exception e) {
-            log.error("error",e);
-            return new OrderedGatewayFilter((exchange, chain) -> chain.filter(exchange),10);
+            } catch (Exception e) {
+                return Mono.error(e);
+            }
+        }
+
+        @Override
+        public int getOrder() {
+            return 10;
         }
     }
+
+
+//    @Override
+//    public GatewayFilter apply(Config config) {
+//        try {
+//            JsonHandleDTO jsonHandleDTO = objectMapper.readValue(config.getConfiguration(),JsonHandleDTO.class);
+//            JsonNode jsonSchema
+//                    = jsonHandleDTO.getRequestJsonSchema();
+//            JsonSchema validateSchema = jsonSchemaInlineFactory.getJsonSchema(jsonSchema);
+//            return new OrderedGatewayFilter(((exchange, chain) -> {
+//                ServerRequest serverRequest = ServerRequest.create(exchange, messageReaders);
+//                return serverRequest.bodyToMono(JsonNode.class).flatMap(jsonNode -> {
+//                    ProcessingReport report =  validateSchema.validateUnchecked(jsonNode);
+//                        if(report.isSuccess()) {
+//                            return chain.filter(exchange);
+//                        }
+//                      return Mono.error(new Exception("error"));
+//                });
+//            }),10);
+//        } catch (Exception e) {
+//            log.error("error",e);
+//            return new OrderedGatewayFilter((exchange, chain) -> return Mono.error(new Exception(e));,10);
+//        }
+//    }
 }
